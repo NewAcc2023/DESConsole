d026e730ec2e9868642636facb8762991cfde7c0
Alex <ttomson979@gmail.com>
2023-11-20 21:08:11 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DESConsole
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			DES_Encryption des = new DES_Encryption();
+			des.Encrypt("0123456789ABCDEF", "133457799BBCDFF1");
+			
+		
+		}
+	}
+}
